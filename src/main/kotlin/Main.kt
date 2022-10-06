6bafce1c7f4e6d605ec239aa304ad6cfabd4918b
ThiagoDuart <jobtigas@gmail.com>
2022-10-05 23:34:35 -0300

    Funçoes basicas prontas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,51 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val contaThiago = Conta("Thiago",1000)
+    val contaIsa = Conta("Isa", 1001)
+    contaThiago.depositar(1000.0)
+    contaIsa.depositar(1000.0)
+    println(contaThiago.saldo)
+    println(contaIsa.saldo)
+
+
+
+
 }
+
+class Conta(val titular:String, val numeroConta:Int) {
+
+    var saldo = 0.0
+        private set
+
+    fun depositar(value: Double) {
+        if (value > 0) {
+            this.saldo += value
+        }
+    }
+
+    fun transferencia(value: Double, destino: Conta): Boolean {
+        if (saldo >= value) {
+            saldo -= value
+            destino.depositar(value)
+            return true
+        }
+            return false
+    }
+    fun saque (value: Double) {
+        if (saldo >= value){
+            saldo -= value
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
